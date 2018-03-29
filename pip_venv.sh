8d3b5cbde7c38a1793825888e2ba9e849e3cd67a
Ray Luo <rayluo.mba@gmail.com>
2018-03-29 08:10:00 +0000

    A script to install pip and virtualenv

---
+++ pip_venv.sh
@@ -0,0 +1,4 @@
+# You might need to run this script by sudo, for pip to be installed globally
+wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
+python /tmp/get-pip.py
+pip install virtualenv
