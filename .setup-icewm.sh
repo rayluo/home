5610a0a8dcd16f93f6f110735bdcc1037f5d46fd
Ray Luo <rayluo.mba@gmail.com>
2020-05-17 15:15:40 -0700

    Leave Ctrl+Space for tmux's copy&paste text selection

--- .setup-icewm.sh
+++ .setup-icewm.sh
@@ -7,6 +7,9 @@ sed -e "s/^[# ]*KeySysTileVertical.*/KeySysTileVertical=\"Alt+Ctrl+V\"/g" -i ~/.
 # Its default setting Alt+Shift+F? tends to conflict with other apps
 sed -e "s/^[# ]*KeySysTileHorizontal.*/KeySysTileHorizontal=\"Alt+Ctrl+H\"/g" -i ~/.icewm/preferences
 
+# Leave Ctrl+Space alone. Tmux uses it for text selection in copy-and-paste mode.
+sed -e "s/^key \"Ctrl+Space\"/# key \"Ctrl+Space\"/g" -i ~/.icewm/keys
+
 # Trigger current icewm session to reload the settings
 killall -SIGHUP icewm
 
